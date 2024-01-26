41abfd312fa583d0a21bd3217f79ee823ae6c334
anderson <andersonpbrotas@gmail.com>
2024-01-26 11:09:06 -0300

    feat: courses progress mock

---
+++ components/CoursesProgress/Info.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="information" v-if="!status">
+        <h4>Nenhum curso em andamento</h4>
+        <p>Não há progresso em nenhum curso ainda. Considere se inscrever em um.</p>
+        <NuxtLink to="">Inscrever-se</NuxtLink>    
+    </div>
+    <div class="information" v-else>
+        <h4>Diversidade da equipe</h4>
+        <p>Projetado para promover a inclusividade e perspectivas diversas.</p>
+        <NuxtLink to="">Retomar curso</NuxtLink>    
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        status: {
+            type: Boolean,
+            required: true
+        }
+    })
+</script>
--- components/CoursesProgress/Pie.vue
+++ components/CoursesProgress/Pie.vue
@@ -1,3 +1,21 @@
 <template>
-    <div class="progress"></div>
+    <div class="pie" v-if="status">
+        <figure class="progress-circle"></figure>
+        <figure class="progress-bar"></figure>
+        <span class="score">25%</span>
+    </div>
+    <div class="pie empty" v-else>
+        <figure class="progress-circle"></figure>
+        <figure class="progress-bar"></figure>
+        <span class="score">0%</span>
+    </div>
 </template>
+
+<script setup>
+    const props = defineProps({
+        status: {
+            type: Boolean,
+            required: true
+        }
+    })
+</script>
